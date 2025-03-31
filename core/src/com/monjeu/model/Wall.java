e05140f238cc89e298c054d8e60f002f8bff9810
ecrou-exact <geffetheo@gmail.com>
2025-03-31 22:06:26 +0200

    Pacman_Animation_et_Bloc

---
+++ core/src/com/monjeu/model/Wall.java
@@ -0,0 +1,28 @@
+package com.monjeu.model;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.monjeu.view.TextureFactory;
+
+public class Wall extends GameElement {
+	private Rectangle hitBoxMur;
+
+	public Wall(int x, int y) {
+        super(x, y , GameConstants.WALL_SIZE,GameConstants.WALL_SIZE);
+        hitBoxMur =  new Rectangle(x, y, GameConstants.WALL_SIZE, GameConstants.WALL_SIZE);
+    }
+	public Rectangle getHitBox() {
+		return hitBoxMur;
+	}
+
+	@Override
+	public void update(float delta) {
+		hitBoxMur.setPosition(getX(), getY());
+	}
+
+	@Override
+	public void render(SpriteBatch batch) {
+		batch.draw(TextureFactory.getInstance().getBloc(),this.getX(), this.getY(), GameConstants.WALL_SIZE, GameConstants.WALL_SIZE);
+	}
+
+}

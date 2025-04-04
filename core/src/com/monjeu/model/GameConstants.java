0f1019c1cece296bedc2f7d18c8d5b9217b45b78
origine-neutre <mathias@geffe.fr>
2025-04-04 22:47:16 +0200

    coorection_bug

--- core/src/com/monjeu/model/GameConstants.java
+++ core/src/com/monjeu/model/GameConstants.java
@@ -42,7 +42,7 @@ public final class GameConstants {
     /**
      * Vitesse de pacman en unités par seconde.
      */
-    public static final float PACMAN_SPEED = 200.0f;
+    public static final float PACMAN_SPEED = 200.0f;		// La vitesse ne marche plus, pacman est bloqué à une vitesse
 
     /**
      * Titre du jeu affiché dans la fenêtre.

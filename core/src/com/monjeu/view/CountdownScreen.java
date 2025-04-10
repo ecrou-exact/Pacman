8651cf9bf7738d45e54e378665bf85dedeb4f3cf
origine-neutre <mathias@geffe.fr>
2025-04-10 10:12:52 +0200

    coorection_bug

--- core/src/com/monjeu/view/CountdownScreen.java
+++ core/src/com/monjeu/view/CountdownScreen.java
@@ -13,7 +13,8 @@ public class CountdownScreen implements Screen {
     private MainGame mainGame;
     private SpriteBatch batch;
     private BitmapFont font;
-    private int countdown = 3; // Commence à 3
+    /* private int countdown = 3; // Commence à 3 */
+    private boolean showGO = true;
 
     public CountdownScreen(MainGame game) {
         this.mainGame = game;
@@ -26,20 +27,24 @@ public class CountdownScreen implements Screen {
         Timer.schedule(new Timer.Task() {
             @Override
             public void run() {
-                countdown--;
+                /* countdown--;
                 if (countdown < 0) {
                     mainGame.startGame(); // Démarrer le jeu après le décompte
-                }
+                } */
+            	showGO = false;
+            	mainGame.startGame();
             }
-        }, 1f, 1f, 3); // Délai initial 1s, répétition 1s, exécute 3 fois
+        }, /* 1f, 1f, 3 */ 1f); // Délai initial 1s, répétition 1s, exécute 3 fois
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        if (!showGO) return; //Ne rien afficher
+        
         batch.begin();
-
-        String text = (countdown > 0) ? String.valueOf(countdown) : "GO!";
+        String text = "GO!";
+        /* String text = (countdown > 0) ? String.valueOf(countdown) : "GO!";*/
         GlyphLayout layout = new GlyphLayout(font, text);
         float x = (Gdx.graphics.getWidth() - layout.width) / 2;
         float y = Gdx.graphics.getHeight() / 2;

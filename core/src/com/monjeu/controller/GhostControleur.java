a8f5df8c046323d705fc4396a51f652cdabd7d72
origine-neutre <mathias@geffe.fr>
2025-04-27 15:36:05 +0200

    correction_bug

--- core/src/com/monjeu/controller/GhostControleur.java
+++ core/src/com/monjeu/controller/GhostControleur.java
@@ -2,16 +2,23 @@ package com.monjeu.controller;
 
 import com.monjeu.model.GameElement;
 import com.monjeu.model.Ghost;
-import com.monjeu.model.Wall;
 import com.monjeu.model.World;
+//import com.monjeu.model.Pacman;
+//import java.util.ArrayList;
+//import java.util.Random;
 
 public class GhostControleur {
-
 	private World world;
+	//private Ghost ghost;
+	//private Pacman pacman;
+	//private Random random = new Random();
 	
-	public GhostControleur(World world) {
+	public GhostControleur(World world) { //, Ghost ghost, Pacman pacman
 		this.world = world;
+		//this.ghost = ghost;
+		//this.pacman = pacman;
 	}
+	
 	public void update(float delta) {
 		for (GameElement element : world.getGameElements()) {
             if (element instanceof Ghost) {
@@ -19,5 +26,7 @@ public class GhostControleur {
             }
         }
     }
-
+	
+	//private void moveRandomly(Ghost ghost) {
+	//}
 }
--- core/src/com/monjeu/model/World.java
+++ core/src/com/monjeu/model/World.java
@@ -33,8 +33,8 @@ public class World {
      */
     public World() {
         gameElements = new Array<GameElement>();
-        loadLevel("levels/level1.json");
-        // loadLevel("levels/level2.json");
+        // loadLevel("levels/level1.json");
+        loadLevel("levels/level2.json");
     }
     
     /**
